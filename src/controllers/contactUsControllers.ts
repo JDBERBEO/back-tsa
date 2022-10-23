98d786b33be7aaef21e8538702890fcf02fb3620
Juan David Berbeo Escobar <jberbeo@estratec.local>
2022-10-23 15:49:55 -0500

    feat: setups contact us dynamic user info

--- src/controllers/contactUsControllers.ts
+++ src/controllers/contactUsControllers.ts
@@ -34,8 +34,7 @@ export const postContactUs = async (req: Request, res: Response) => {
     const { body } = req
     const contactUsReport = await ContactUs.create(body)
     res.status(201).send({ contactUsReport });
-    console.log('llegué hasta antes del llamdo')
-    await newContactUs('meretracto2022@gmail.com', 'Pedro', 'pedro@gmail.com', 'esto es una prueba')
+    await newContactUs(process.env.MAILER_USER, contactUsReport.name, contactUsReport.email, contactUsReport.message)
   } catch (error) {
     res.json(error)
   }
