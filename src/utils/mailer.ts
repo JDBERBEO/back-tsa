3fdab48a172349f4423b91fd78c3cb28eb9048ca
Juan David Berbeo Escobar <jberbeo@estratec.local>
2022-10-23 14:59:03 -0500

    feat: add nodemailer

---
+++ src/utils/mailer.ts
@@ -0,0 +1,46 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth:{
+      user:process.env.MAILER_USER,
+      pass:process.env.MAILER_PASS
+  }
+})
+
+async function verify(){
+try{
+  const connection = await transporter.verify()
+  if(connection){
+          console.log("Server ready to take messages");
+      }
+  }catch(error){
+      console.log(error)
+  }
+}
+async function newContactUs({email, name, userEmail, message}){
+  await transporter.sendMail({
+      from: `"${process.env.MAILER_USER}"`,
+      to: email,
+      subject: "Nuevo Mensaje de Contáctenos",
+      html: `
+      <div>
+          <h1>Hola Admin,</h1>
+          <p>
+          Ha llegado un nuevo mensaje del área de contáctenos con la siguiente información</p><br/>
+          <p>Nombre: ${name}</p><br/>
+          <p>Email: ${userEmail}</P><br/>
+          <p>Mensaje: ${message}</p><br/>
+      </div>
+      `,
+      })
+  }
+
+
+  module.exports = {
+      transporter,
+      verify,
+      newContactUs
+}
