0c14cec8d9ae4c21496fa17670019d9e460b28fd
Juan David Berbeo Escobar <jberbeo@estratec.local>
2022-10-25 21:41:01 -0500

    'c'

--- src/routes/customerRoutes.ts
+++ src/routes/customerRoutes.ts
@@ -12,7 +12,7 @@ const customerRouter = express.Router();
 customerRouter.get('/', getClaims);
 customerRouter.post('/transactionInfo', transactionInfo);
 customerRouter.post('/:id', postPreviousCheckClaim)
-customerRouter.get('/:transactionId', getClaimByTransactionId)
+customerRouter.get('/getClaim/:transactionId', getClaimByTransactionId)
 // customerRouter.post('/:id', postClaimRender);
 
 // TODO: Create auth middleware
