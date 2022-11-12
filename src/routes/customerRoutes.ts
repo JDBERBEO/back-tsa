f9351668cc258e8c8f82d9e12f6b5157480803f6
Juan David Berbeo Escobar <jberbeo@estratec.local>
2022-11-12 17:39:10 -0500

    'c'

--- src/routes/customerRoutes.ts
+++ src/routes/customerRoutes.ts
@@ -1,19 +1,17 @@
 import express from 'express';
 import {
   getClaims,
-  postClaimRender,
   transactionInfo,
   postPreviousCheckClaim,
-  getClaimByTransactionId
+  getClaimByTransactionId,
 } from '../controllers/customerControllers';
 
 const customerRouter = express.Router();
 
 customerRouter.get('/', getClaims);
 customerRouter.post('/transactionInfo', transactionInfo);
-customerRouter.post('/:id', postPreviousCheckClaim)
-customerRouter.get('/getClaim/:transactionId', getClaimByTransactionId)
+customerRouter.post('/:id', postPreviousCheckClaim);
+customerRouter.get('/getClaim/:transactionId', getClaimByTransactionId);
 // customerRouter.post('/:id', postClaimRender);
 
-// TODO: Create auth middleware
 export default customerRouter;
