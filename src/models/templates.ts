4e3940f4fd2c52a11e823b9a30c5bba3652bad19
JDBERBEO <47923495+JDBERBEO@users.noreply.github.com>
2024-06-02 23:14:19 -0500

    Develop (#31)
    * feat: get cloudinary url res
    
    * sync (#12)
    
    * feat: get cloudinary url res (#11)
    
    Co-authored-by: Juan David Berbeo Escobar <jberbeo@estratec.local>
    
    * add clgs
    
    * clg transaction
    
    * feat: delete console logs
    
    Co-authored-by: Juan David Berbeo Escobar <jberbeo@estratec.local>
    
    * sync (#13)
    
    * feat: get cloudinary url res (#11)
    
    Co-authored-by: Juan David Berbeo Escobar <jberbeo@estratec.local>
    
    * add clgs
    
    * clg transaction
    
    * feat: delete console logs
    
    ---------
    
    Co-authored-by: Juan David Berbeo Escobar <jberbeo@estratec.local>
    
    * feat: delete files from previus claim (#15)
    
    * feat: create update claim with files controller
    
    * feat; add formatBytes
    
    * feat: return claim
    
    * feat add logs
    
    * feat: update claim
    
    * feat: update proofs in claim
    
    * feat: test error creating claim
    
    * feat: delete error
    
    * feat: add email with templates
    
    * add paramter
    
    * solve conflicts again
    
    * feat: add proofs test for mulitple
    
    * feat: change claim replacemant after proofs update
    
    * feat: refator updateClaimFile
    
    * feat: delete commented code
    
    * feat: add claimPrice (#30)
    
    ---------
    
    Co-authored-by: Juan David Berbeo Escobar <jberbeo@estratec.local>

--- src/models/templates.ts
+++ src/models/templates.ts
@@ -6,36 +6,31 @@ interface Template extends Document {
   fileUrl: string;
   fileUid: string;
   description: string;
-  price: number
 }
 
 const TemplateSchema: Schema = new Schema(
   {
-  name: { 
-    type: String, 
-    required: true 
-  },
-  internalCode: { 
-    type: String, 
-    required: true 
-  },
-  fileUrl: { 
-    type: String, 
-    required: true 
-  },
-  fileUid: { 
-    type: String, 
-    required: true 
-  },
-  price: {
-    type: Number,
-    required: true,
-    default: 9000000
-  }
+    name: {
+      type: String,
+      required: true,
+    },
+    internalCode: {
+      type: String,
+      required: true,
+    },
+    fileUrl: {
+      type: String,
+      required: true,
+    },
+    fileUid: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
-  });
+  }
+);
 
 const Template: Model<Template> = model('Template', TemplateSchema);
 
